e277e646cb47340a567172cdd6e66e14f2cfe212
Evgenia S <sdevgenia@gmail.com>
2017-10-02 23:04:08 +0300

    Add implementation of missing checks

--- IBZI_Lab1/ViewController.swift
+++ IBZI_Lab1/ViewController.swift
@@ -21,13 +21,29 @@ class ViewController: UIViewController {
     private var arrayOfSymbols = [String]()
     private var arrayOfCharacters = [String]()
     
-    private func check () {
+    private func check () -> Bool {
         
-        if sizeOfBlockField.hasText && sizeOfBlock >= 1 {
-            print("Size of block has been entered succesfully")
+        if sizeOfBlockField.hasText {
+            for i in sizeOfBlockField.text!.characters {
+                if "0"..."9" ~= i {
+                        print("Size of block has been entered succesfully")
+                    } else {
+                        print("Error: Only digits should be there!")
+                        return false
+                    }
+            }
+        
+            if Int(sizeOfBlockField.text!)! >= 1 && Int(sizeOfBlockField.text!)! <= String(textField.text!).characters.count {
+                sizeOfBlock = Int(sizeOfBlockField.text!)!
+            } else {
+                print("Error: < 1 or > size of text")
+                return false
+            }
+        
         } else {
-            //error
-            print("Error: Size of block field is empty!")
+                //error
+                print("Error: Size of block field is empty!")
+                return false
         }
         
         if textField.hasText {
@@ -49,136 +65,112 @@ class ViewController: UIViewController {
         } else {
             //exception
             print("Error: Text field is empty!")
+            return false
         }
         
         if orderField.hasText {
-            arrayOfSymbols = orderField.text!.components(separatedBy: " ")
+            arrayOfSymbols = []
             
-            for i in arrayOfSymbols {
-                if "0"..."9" ~= i {
-                    print("Order consists of digits only")
+            for i in orderField.text!.components(separatedBy: " ") {
+                if "0"..."9" ~= i && arrayOfSymbols.contains(i) == false && Int(i)! > 0 {
+                    arrayOfSymbols.append(i)
                 } else {
-                    print("Error: Only digits should be there!")
+                    print("Error: повторения")
+                    return false
                 }
             }
             
-            if arrayOfSymbols.count == sizeOfBlock {
+            if arrayOfSymbols.count == sizeOfBlock && Int(arrayOfSymbols.max()!)! <= sizeOfBlock {
                 print("Everything is okay")
             } else {
-                print("Error: Size of array of symbols is incorrect!")
+                print("Error: Array of symbols has been entered with mistakes!")
+                return false
             }
         } else {
             print("Error: Order field is empty!")
+            return false
         }
+        
+        return true
+    }
+    
+    private func encryptedFunction(order: [Int], fragment: [String]) -> String {
+        var result = ""
+        for i in 0...sizeOfBlock - 1 {
+            result += fragment[order[i] - 1]
+        }
+        return result
+    }
+    
+    private func encryptionOfTheMessage() -> String {
+        var resultText = ""
+        var i = 0
+
+        repeat {
+            var fragment = [String]()
+            
+            if i + sizeOfBlock <= arrayOfCharacters.count {
+                for j in 0...sizeOfBlock - 1 {
+                    let index = arrayOfCharacters.index(after: j + i) - 1
+                    print(index)
+                    fragment.append(arrayOfCharacters[index])
+                }
+                i += sizeOfBlock
+            } else {
+                for j in i...arrayOfCharacters.count - 1 {
+                    let index = arrayOfCharacters.index(after: j + i) - 1
+                    print(index)
+                    fragment.append(arrayOfCharacters[index])
+                }
+                i += 1
+            }
+            
+            print("res:", fragment)
+            let resultMessage = encryptedFunction(order: arrayOfSymbols.flatMap({Int($0)}), fragment: fragment)
+            print("Decrypted message: ", resultMessage)
+            resultText += resultMessage
+            
+        } while i < arrayOfCharacters.count
+        
+        return resultText
     }
     
     @IBAction private func encryptionButton(_ sender: UIButton) {
-        sizeOfBlock = Int(sizeOfBlockField.text!)!
-        
-        func cryptTheMessage (order: [Int], fragment: [String]) -> String {
-            var result = ""
-            for i in 0...sizeOfBlock - 1 {
-                result += fragment[order[i] - 1]
-            }
-            return result
-        }
-        
-        check()
-        
-        var resultText = ""
-        var i = 0
-
-        repeat {
-            var fragment = [String]()
-
-            if i + sizeOfBlock <= arrayOfCharacters.count {
-                for j in 0...sizeOfBlock - 1 {
-                    let index = arrayOfCharacters.index(after: j + i) - 1
-                    print(index)
-                    fragment.append(arrayOfCharacters[index])
-                }
-                i += sizeOfBlock
-            } else {
-                for j in i...arrayOfCharacters.count - 1 {
-                    let index = arrayOfCharacters.index(after: j + i) - 1
-                    print(index)
-                    fragment.append(arrayOfCharacters[index])
-                }
-                i += 1
-                }
-            
-            print("res:", fragment)
-            
-            let cryptedMessage = cryptTheMessage(order: arrayOfSymbols.flatMap({Int($0)}), fragment: fragment)
-            print("Crypted message: ", cryptedMessage)
-
-            resultText += cryptedMessage
-            
-        } while i < arrayOfCharacters.count
-        
-        print(resultText)
-        
-        encryptedMessageLabel.text = resultText
-        resultText = ""
         sizeOfBlock = 0
         arrayOfCharacters = []
         arrayOfSymbols = []
-        
+
+        if check() {
+            let resultText = encryptionOfTheMessage()
+            print(resultText)
+            encryptedMessageLabel.text = resultText
+        }
     }
    
     @IBAction func decryptionButton(_ sender: UIButton) {
-        sizeOfBlock = Int(sizeOfBlockField.text!)!
-        
-        func decryptTheMessage (order: [Int], fragment: [String]) -> String {
-            var result = ""
-            for i in 0...sizeOfBlock - 1 {
-                result += fragment[order[i] - 1]
-            }
-            return result
-        }
-
-        check()
-
-        var resultText = ""
-        var i = 0
-        
-        repeat {
-            var fragment = [String]()
-            
-            if i + sizeOfBlock <= arrayOfCharacters.count {
-                for j in 0...sizeOfBlock - 1 {
-                    let index = arrayOfCharacters.index(after: j + i) - 1
-                    print(index)
-                    fragment.append(arrayOfCharacters[index])
-                }
-                i += sizeOfBlock
-            } else {
-                for j in i...arrayOfCharacters.count - 1 {
-                    let index = arrayOfCharacters.index(after: j + i) - 1
-                    print(index)
-                    fragment.append(arrayOfCharacters[index])
-                }
-                i += 1
-            }
-           
-            print("res:", fragment)
-            
-            let decryptedMessage = decryptTheMessage(order: arrayOfSymbols.flatMap({Int($0)}), fragment: fragment)
-            print("Crypted message: ", decryptedMessage)
-            
-            resultText += decryptedMessage
-            
-        } while i < arrayOfCharacters.count
-        
-        print(resultText)
-        
-        decryptedMessageLabel.text = resultText
-        resultText = ""
         sizeOfBlock = 0
         arrayOfCharacters = []
         arrayOfSymbols = []
+        
+        if textField.hasText {
+            print("Input text has been entered succesfully")
+            if  String(textField.text!).characters.count % Int(sizeOfBlockField.text!)!  ==  0 {
+                print("Size is okay")
+                if check() {
+                    let resultText = encryptionOfTheMessage()
+                    print(resultText)
+                    decryptedMessageLabel.text = resultText
+                }
+            } else {
+                decryptedMessageLabel.text = "Error: Decryption is impossible in this case!"
+                print("Error: Decryption is impossible in this case")
+                }
+                print(arrayOfCharacters)
+        } else {
+            //exception
+            print("Error: Text field is empty!")
+        }
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
